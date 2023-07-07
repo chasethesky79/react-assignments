04352459878750f900f9b9f26e4898aa71c9bcc8
chasethesky79 <seshadri.bharath@yahoo.com>
2023-07-06 21:43:17 -0500

    Implemented validation errors

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
 import React, { useState } from 'react'
+import styles from './App.css'
 
 function App() {
-  const [state, setState] = useState({
+  const intiialState = {
     username: '',
     password: '',
     passwordCombination: '',
     email: '',
     errors: []
-  })
+  };
+  const [state, setState] = useState(intiialState)
   const { username, password, passwordCombination, email, errors } = state;
-  const handleChange = (e) =>  setState({...state, [e.target.name]: e.target.value});
-  const submitForm = () => console.log(`CURRENT STATE ${JSON.stringify(state)}`);
+  const handleChange = (e) =>  {
+    const {target: { name, value } } = e;
+    if (!value) {
+      const errorMsg = `${name} is required`;
+      setState({ ...state, errors: [ ...state.errors, errorMsg ]})
+      return;
+    }
+    setState({...state, [name]: value });
+  }
+  const submitForm = (e) => {
+    e.preventTarget();
+    setState(intiialState)
+  }
   return (
-    <div>
-      <h1>Create Account</h1>
-      Username: <input type='text' name='username' value={username} onChange={handleChange}/><br/>
-      Password: <input type='text' name='password' value={password} onChange={handleChange}/><br/>
-      Password Combination: <input name='passwordCombination' type='text' value={passwordCombination} onChange={handleChange}/><br/>
-      Email: <input type='text' name='email' value={email} onChange={handleChange}/><br/>
-      <br/>
-      <button onClick={submitForm}>Submit</button>
-    </div>
+      <div>
+        <h1>Create Account</h1>
+        { state.errors.map((error, index) => <div key={`err-${index}`} className={styles.errors}>{error}</div>)}
+        Username: <input type='text' name='username' value={username} onBlur={handleChange}/><br/>
+        Password: <input type='text' name='password' value={password} onBlur={handleChange}/><br/>
+        Password Combination: <input name='passwordCombination' type='text' value={passwordCombination} onBlur={handleChange}/><br/>
+        Email: <input type='text' name='email' value={email} onBlur={handleChange}/><br/>
+        <br/>
+        <button disabled={state.errors.length > 0} onClick={submitForm}>Submit</button>
+      </div>
   );
 }
 
